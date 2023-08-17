94889e14acdcd928614d083b4a684f3bde47492c
juanjoelo <juanjoel.olguin@gmail.com>
2023-08-17 13:12:43 -0300

    Initial commit

---
+++ src/loginmain/LoginMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginmain;
+
+/**
+ *
+ * @author juan_
+ */
+public class LoginMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaLogin ventana = new VentanaLogin();
+                
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        
+    }
+    
+}
